9398ef1984ff236608f7803bd3eab0029d823250
= <=>
2019-10-12 10:36:50 +0700

    Check auth works or not in Laravel

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -8,13 +8,18 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
+     * Display a listing of your object.
+     * For example: You want to get list of books. This controller should be called BookController.php
+     * And then in method @index. You will load all books here.
+     * You should do some steps below:
+     * - Prepare view(blade php) from view folder
+     * - Using method ->with() or ->compact() to pass your list, object to view
+     * - Return it.
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('index');
     }
 
     /**
