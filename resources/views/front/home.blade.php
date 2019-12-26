28259923b406309351a530108b4da7c99646707b
lorence <lorence@enclave.vn>
2019-12-26 21:43:38 +0700

    Optimize code

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <title>Vogo Book</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="title" content="Vogo Book"/>
+    <meta name="referrer" content="always">
+    <meta name="description" itemprop='description' content='This system is not like a nomal bookstore system. It forcus on selling 100 bestseller books that were gotten from amazon. In addition, this system have some feature that help shop owner can attract customer use the mobile application to buy books in system.' />
+    <meta name="keywords" content="Book, buy book" />
+    <meta name="author" content="" />
+    <meta name="theme-color" content="">
+
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta property="fb:app_id" content=""/>
+
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="parsely-link" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="U6fpFebZWSlTvm3btVc5VQeCZKJ1OLQ4RQFju35N_ig" />
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/gijgo.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/slicknav.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('front2/css/style.css') }}">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="front2/img/logo.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="single_slider  d-flex align-items-center slider_bg_1">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-7 col-md-6">
+                        <div class="slider_text ">
+                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >Online Bookstore System</h3>
+                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">Make your life successful with reading books</p>
+                            <div class="video_service_btn wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">
+                                <a href="#" class="boxed-btn3">Get Start Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-md-6">
+                        <div class="phone_thumb wow fadeInDown" data-wow-duration="1.1s" data-wow-delay=".2s">
+                            <img src="front2/img/ilstrator/phone.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- JS here -->
+    <script src="{{ URL::asset('front2/js/vendor/modernizr-3.5.0.min.js') }}"></script>
+    <script src="{{ URL::asset('front2/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ URL::asset('front2/js/popper.min.js') }}"></script>
+
+    <script src="{{ URL::asset('front2/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/popper.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/bootstrap.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/ajax-form.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/waypoints.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/scrollIt.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/wow.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/nice-select.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.slicknav.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/plugins.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/gijgo.min.js')}}"></script>
+
+    <!--contact js-->
+    <script src="{{ URL::asset('front2/js/contact.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.ajaxchimp.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.form.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/jquery.validate.min.js')}}"></script>
+    <script src="{{ URL::asset('front2/js/mail-script.js')}}"></script>
+
+    <script src="{{ URL::asset('front2/js/main.js')}}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.home');
 });
 
 Route::get('/books/{slug}.html', 'HomeController@pages')->name('home.pages');
